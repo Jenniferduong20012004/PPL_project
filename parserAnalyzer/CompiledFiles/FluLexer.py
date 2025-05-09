416403f52a9dfdc2b756e1005d95ab3d4821930a
thaortrinh <nghthtr5536@gmail.com>
2025-05-09 21:38:45 +0700

    chin | change to pink theme

--- parserAnalyzer/CompiledFiles/FluLexer.py
+++ parserAnalyzer/CompiledFiles/FluLexer.py
@@ -1,4 +1,4 @@
-# Generated from F:\\PPL\\PPL_project\\parserAnalyzer\\Flu.g4 by ANTLR 4.9.2
+# Generated from C:\Users\TRINH\OneDrive\Máy tính\ppl_project\PPL_project\parserAnalyzer\Flu.g4 by ANTLR 4.9.2
 from antlr4 import *
 from io import StringIO
 import sys
--- parserAnalyzer/run.py
+++ parserAnalyzer/run.py
@@ -38,7 +38,7 @@ def runTest():
             print(f"Input rejected: {msg}")
             exit(1)  # Exit the program with an error
 
-    filename = '002.txt'
+    filename = '001.txt'
     inputFile = os.path.join(DIR, './tests', filename)    
 
     print('List of token: ')
