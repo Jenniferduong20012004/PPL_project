6424598ba793f3b749ba16aa602b1577b624e92b
Itsjennyyahh <56812916+Jenniferduong20012004@users.noreply.github.com>
2025-05-22 17:55:32 +0700

    add

--- module/utilFunction.py
+++ module/utilFunction.py
@@ -1,8 +1,14 @@
-from Database import Database
-class utilFunction:
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from module.Database import Database
+class utilFunction():
     def __init__(self):
         self.databaseCon = Database();
     def  getPeriodCycle(self):
         return ("phuc oi lam cho toi")
     def getPeriodForMonth(self):
         return ("nghi oi lam cho toi")
+    def requireStart (self, dateType, date):
+        print (dateType)
+        return "akb"
